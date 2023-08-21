208ae9292bc0a532fb55dd3021efa3ab6aadaf93
Romário Abílio <113223503+romarioabilio@users.noreply.github.com>
2023-08-20 21:01:06 -0300

    att

--- src/main/java/br/edu/ufape/poo/barbeariaufape/BarbeariaufapeApplication.java
+++ src/main/java/br/edu/ufape/poo/barbeariaufape/BarbeariaufapeApplication.java
@@ -1,4 +1,4 @@
-package br.edu.ufape.poo.barbeariaufape.barbeariaufape;
+package br.edu.ufape.poo.barbeariaufape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
