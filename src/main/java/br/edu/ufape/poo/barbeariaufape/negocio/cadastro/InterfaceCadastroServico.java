832967bb4b4075fc4a7b278a69bde22bfa3eb087
Romário Abílio <113223503+romarioabilio@users.noreply.github.com>
2023-08-23 23:44:47 -0300

    Criando InterfaceCadastroServico

---
+++ src/main/java/br/edu/ufape/poo/barbeariaufape/negocio/cadastro/InterfaceCadastroServico.java
@@ -0,0 +1,25 @@
+package br.edu.ufape.poo.barbeariaufape.negocio.cadastro;
+
+import java.util.List;
+
+
+import br.edu.ufape.poo.barbeariaufape.negocio.basica.Servico;
+import br.edu.ufape.poo.barbeariaufape.negocio.cadastro.exception.ServicoDuplicadoException;
+import br.edu.ufape.poo.barbeariaufape.negocio.cadastro.exception.ServicoNaoExisteException;
+
+
+public interface InterfaceCadastroServico {
+
+	Servico procurarServicoNome(String nome) throws ServicoNaoExisteException;
+
+	Servico salvarServico(Servico entity) throws ServicoDuplicadoException;
+
+	List<Servico> listarServicos();
+
+	boolean verificarExistenciaServicoId(Long id);
+
+	Servico localizarServicoId(Long id);
+
+	void removerServicoNome(String nome) throws ServicoNaoExisteException;
+
+}
