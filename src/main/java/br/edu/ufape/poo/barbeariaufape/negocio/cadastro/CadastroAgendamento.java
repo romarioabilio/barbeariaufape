6552904127ae96870f28f728ec1c51c947cc2d72
pedrowillliam <pedrowilliam014@hotmail.com>
2023-08-24 01:07:29 -0300

    Criando CadastroAgendamento

--- src/main/java/br/edu/ufape/poo/barbeariaufape/negocio/cadastro/CadastroAgendamento.java
+++ src/main/java/br/edu/ufape/poo/barbeariaufape/negocio/cadastro/CadastroAgendamento.java
@@ -1,5 +1,35 @@
 package br.edu.ufape.poo.barbeariaufape.negocio.cadastro;
 
-public class CadastroAgendamento {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import br.edu.ufape.poo.barbeariaufape.negocio.basica.Agendamento;
+import br.edu.ufape.poo.barbeariaufape.dados.InterfaceColecaoAgendamento;
+
+@Service
+public class CadastroAgendamento implements InterfaceCadastroAgendamento {
+
+    private final InterfaceColecaoAgendamento interfaceColecaoAgendamento;
+
+    @Autowired
+    public cadastroAgendamento(InterfaceColecaoAgendamento interfaceColecaoAgendamento) {
+        this.interfaceColecaoAgendamento = interfaceColecaoAgendamento;
+    }
+
+    @Override
+    public Agendamento salvarAgendamento(Agendamento agendamento) {
+        return interfaceColecaoAgendamento.save(agendamento);
+    }
+
+    @Override
+    public Agendamento consultarAgendamento(Long id) {
+        return interfaceColecaoAgendamento.findById(id).orElse(null);
+    }
+
+    @Override
+    public void excluirAgendamento(Long id) {
+        interfaceColecaoAgendamento.deleteById(id);
+    }
+    
     
 }
+
