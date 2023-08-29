854442a0a1e1ac4b447f020ab63ee4957fa81501
FabioGMedeiros <fabio.filho2001@hotmail.com>
2023-08-29 17:10:05 -0300

    Finalizando Crud Agendamento

--- src/main/java/br/edu/ufape/poo/barbeariaufape/negocio/cadastro/InterfaceCadastroAgendamento.java
+++ src/main/java/br/edu/ufape/poo/barbeariaufape/negocio/cadastro/InterfaceCadastroAgendamento.java
@@ -1,12 +1,8 @@
 package br.edu.ufape.poo.barbeariaufape.negocio.cadastro;
 
 import br.edu.ufape.poo.barbeariaufape.negocio.basica.Agendamento;
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 import java.util.List;
-
-
 
 public interface InterfaceCadastroAgendamento {
 
@@ -16,7 +12,4 @@ public interface InterfaceCadastroAgendamento {
 	void deletarAgendamento(Long id);
 	Agendamento atualizarAgendamento(Long id, Agendamento agendamento);
 	void atualizarDados(Agendamento novoAgendamento, Agendamento agendamento);
-	List<LocalTime> horariosDisponiveis(Long id, LocalDate data);
-	Boolean encaixeHorario(String horarioEscolhidoInicio, List<String> horariosDisponiveis, List<Long> listaServicos);
-
 }
