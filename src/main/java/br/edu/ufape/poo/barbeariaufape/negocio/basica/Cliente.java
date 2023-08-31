22cfe997ccc2c9d301b01e0b3ec7d2c596348198
Romário Abílio <113223503+romarioabilio@users.noreply.github.com>
2023-08-31 00:01:50 -0300

    att Cliente

--- src/main/java/br/edu/ufape/poo/barbeariaufape/negocio/basica/Cliente.java
+++ src/main/java/br/edu/ufape/poo/barbeariaufape/negocio/basica/Cliente.java
@@ -17,10 +17,9 @@ public class Cliente extends Pessoa {
 
     }
 
-    public Cliente(String nome, String cpf, String telefone, Endereco endereco, boolean vip) {
+    public Cliente(String nome, String cpf, String telefone, Endereco endereco) {
         super(nome, cpf, telefone, endereco);
     }
-
 
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     @JsonIgnore
